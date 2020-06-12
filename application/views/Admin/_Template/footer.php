07f80ee439c4decc6960a2c2419f30fbbbf7b514
sgnghrno <sgnghrno@gmail.com>
2020-06-13 02:34:57 +0700

    Create footer.php

---
+++ application/views/Admin/_Template/footer.php
@@ -0,0 +1,13 @@
+ <?php
+  //$getUser = $this->session->userdata('session_user');
+  //$getGrup = $this->session->userdata('session_grup');
+  ?>
+ <!-- Footer -->
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright &copy; KoperasiMahasiswa 2020</span>
+          </div>
+        </div>
+      </footer>
+      <!-- End of Footer -->
